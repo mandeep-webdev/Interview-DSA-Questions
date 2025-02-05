98f19006823d53ebe3aa86eea8c7227049f4eaa0
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-02-05 14:27:19 +0530

    Update connectedUndirectedGraph.java

--- Graph/DFS/connectedUndirectedGraph.java
+++ Graph/DFS/connectedUndirectedGraph.java
@@ -1,12 +1,15 @@
 //Time Complexity: O(V + E)
-// Space Complexity: O(V) 
+// Space Complexity: O(V) => stack space in case of skewed graph is O(V) + visited array O (V)  + resulted list O(V) = 3 O(V) = O(V)
+
 class Solution {
-    public void dfs(ArrayList<Integer> result , ArrayList<ArrayList<Integer>> adj, int[] visited, int node){
-        if(visited[node] == 0){
-            visited[node] = 1;
-            result.add(node);
-            for(int neighbour : adj.get(node)){
-                dfs(result,adj,visited,neighbour);
+    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,int[] visited,ArrayList<Integer>result){
+        visited[node] = 1;
+        result.add(node);
+        
+        for(int neighbour : adj.get(node)){
+            // if node is not visited , process it
+            if(visited[neighbour] == 0) {
+                dfs(neighbour,adj,visited,result);
             }
         }
     }
@@ -15,8 +18,7 @@ class Solution {
         int V = adj.size();
         int[] visited = new int[V];
         ArrayList<Integer> result = new ArrayList<>();
-        dfs(result,adj,visited,0);
+        dfs(0,adj,visited,result);
         return result;
-        
     }
 }
