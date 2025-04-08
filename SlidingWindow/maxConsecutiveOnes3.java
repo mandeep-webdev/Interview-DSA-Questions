6482c3d1002f7c9e8a06408835a6c7cf3b567ad7
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-04-08 13:03:34 +0530

    Update maxConsecutiveOnes3.java

--- SlidingWindow/maxConsecutiveOnes3.java
+++ SlidingWindow/maxConsecutiveOnes3.java
@@ -1,4 +1,36 @@
+// ----- sliding window---- TC--O(n) and SC-- O(1)
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+     int l = 0;
+     int n = nums.length;
+     int zeros = 0;
+     int maxL = 0;
 
+     for(int r = 0; r<n; r++){
+        // if element is zero
+        if(nums[r] == 0) {
+            zeros ++;
+        }
+        // if window becomes invalid, shrink it
+        if(zeros > k) {
+            while(zeros > k){
+                if(nums[l] == 0){
+                    zeros --;
+                   
+                }
+                 l++;
+
+            }
+
+        }
+        // if it is a valid window
+
+        maxL = Math.max(maxL, r-l + 1);
+
+     }
+     return maxL;
+    }
+}
 // Brute Force -------- TC -- O(n2) and SC -- O(1)
 class Solution {
     public int longestOnes(int[] nums, int k) {
