2993685ed9ed1e3a98c6d7db3ff4b0ce335e77fc
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-04-11 15:03:02 +0530

    Update numberOfSubstringsContainsAllThreeCharacters.java

--- SlidingWindow/numberOfSubstringsContainsAllThreeCharacters.java
+++ SlidingWindow/numberOfSubstringsContainsAllThreeCharacters.java
@@ -1,4 +1,29 @@
 //https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+class Solution {
+    public int numberOfSubstrings(String s) {
+     
+      int n = s.length();
+      int result = 0;
+      int l = 0;
+      int[] count = new int[3];
+
+      for(int r=0;r<n;r++){
+        char ch = s.charAt(r);
+        count[ch - 'a'] ++;
+        while(count[0] > 0 && count[1] > 0 && count[2] > 0){
+            // its a valid substring
+            result+=n-r;
+             count[s.charAt(l) - 'a'] -- ;
+             l++;
+        }
+       
+
+      }
+      return result;
+
+      
+    }
+}
 // Brute Force TC-- O(n2) SC--O(3)
 class Solution {
     public int numberOfSubstrings(String s) {
