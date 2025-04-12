d2a61bdb3762042b1fef6e1b380c14a6cbd74716
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-04-12 12:21:14 +0530

    Update numberOfSubstringsContainsAllThreeCharacters.java

--- SlidingWindow/numberOfSubstringsContainsAllThreeCharacters.java
+++ SlidingWindow/numberOfSubstringsContainsAllThreeCharacters.java
@@ -1,51 +1,57 @@
 //https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+// TC -- O(n) SC--O(3)
 class Solution {
     public int numberOfSubstrings(String s) {
-     
-      int n = s.length();
-      int result = 0;
-      int l = 0;
-      int[] count = new int[3];
+     int n = s.length();
+     int l = 0;
+     // count substrings
+     int count = 0;
+     // tract freq of characters
+     int[] arr = new int[3];
 
-      for(int r=0;r<n;r++){
+     for(int r=0; r<n; r++){
         char ch = s.charAt(r);
-        count[ch - 'a'] ++;
-        while(count[0] > 0 && count[1] > 0 && count[2] > 0){
-            // its a valid substring
-            result+=n-r;
-             count[s.charAt(l) - 'a'] -- ;
-             l++;
+        arr[ch-'a'] ++ ;
+         // shrink from left while window contains a, b, c
+        while(arr[0] > 0 && arr[1] > 0 && arr[2] > 0 ){
+             count = count + n - r;
+            // shrink the window to count min valid substring
+            arr[s.charAt(l) - 'a'] -- ;
+            l++;
         }
-       
 
-      }
-      return result;
-
-      
+     }
+     return count;
+     
     }
 }
 // Brute Force TC-- O(n2) SC--O(3)
 class Solution {
     public int numberOfSubstrings(String s) {
-      // brute force
-      int n = s.length();
-      int result = 0;
+     // brute force
+     int n = s.length();
+     // counting the total no. of substring
+     int count = 0;
 
-      // find all substrings
+     // generate all substrings
 
-      for(int i =0; i<n; i++){
-        int[] count = new int[3];
+     for(int i = 0 ; i<n ;i++) {
+        // track freq of characters
+        int[] arr = new int[3];
         for(int j=i; j<n; j++){
-            char c = s.charAt(j);
-            count[c-'a']++;
-            if(count[0] > 0 && count[1] > 0 && count[2] > 0){
-                // find a valid substring
-                result ++;
-
+            char ch = s.charAt(j);
+            arr[ch - 'a'] = 1;
+            if(arr[0] > 0 && arr[1] > 0 && arr[2] > 0){
+                // a valid substring
+                count = count + (n - j);
+                break;
             }
 
         }
-      }
-      return result;
+
+     }
+     return count;
+
+     
     }
 }
